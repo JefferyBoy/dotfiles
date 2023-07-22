bde3c6dd6377cc3cea03922a139bc222a1ea49a4
jeffery <v-leijianfang@meizu.com>
2023-07-22 11:17:18 +0800

    修改lsp

--- .config/nvim/lua/plugins/configs/lspconfig.lua
+++ .config/nvim/lua/plugins/configs/lspconfig.lua
@@ -1,74 +1,116 @@
 local present, lspconfig = pcall(require, "lspconfig")
 
 if not present then
-   return
+  return
 end
 
 require("base46").load_highlight "lsp"
+require "nvchad_ui.lsp"
 
 local M = {}
 local utils = require "core.utils"
 
-require "ui.lsp"
+-- export on_attach & capabilities for custom lspconfigs
 
 M.on_attach = function(client, bufnr)
-   client.resolved_capabilities.document_formatting = false
-   client.resolved_capabilities.document_range_formatting = false
+  client.server_capabilities.documentFormattingProvider = false
+  client.server_capabilities.documentRangeFormattingProvider = false
 
-   local lsp_mappings = utils.load_config().mappings.lspconfig
-   utils.load_mappings({ lsp_mappings }, { buffer = bufnr })
+  utils.load_mappings("lspconfig", { buffer = bufnr })
 
-   if client.server_capabilities.signatureHelpProvider then
-      require("nvchad.ui.signature").setup(client)
-   end
+  if client.server_capabilities.signatureHelpProvider then
+    require("nvchad_ui.signature").setup(client)
+  end
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
+M.capabilities = vim.lsp.protocol.make_client_capabilities()
 
-capabilities.textDocument.completion.completionItem = {
-   documentationFormat = { "markdown", "plaintext" },
-   snippetSupport = true,
-   preselectSupport = true,
-   insertReplaceSupport = true,
-   labelDetailsSupport = true,
-   deprecatedSupport = true,
-   commitCharactersSupport = true,
-   tagSupport = { valueSet = { 1 } },
-   resolveSupport = {
-      properties = {
-         "documentation",
-         "detail",
-         "additionalTextEdits",
-      },
-   },
+M.capabilities.textDocument.completion.completionItem = {
+  documentationFormat = { "markdown", "plaintext" },
+  snippetSupport = true,
+  preselectSupport = true,
+  insertReplaceSupport = true,
+  labelDetailsSupport = true,
+  deprecatedSupport = true,
+  commitCharactersSupport = true,
+  tagSupport = { valueSet = { 1 } },
+  resolveSupport = {
+    properties = {
+      "documentation",
+      "detail",
+      "additionalTextEdits",
+    },
+  },
 }
 
-lspconfig.sumneko_lua.setup {
-   on_attach = M.on_attach,
-   capabilities = capabilities,
+lspconfig.lua_ls.setup {
+  on_attach = M.on_attach,
+  capabilities = M.capabilities,
 
-   settings = {
-      Lua = {
-         diagnostics = {
-            globals = { "vim" },
-         },
-         workspace = {
-            library = {
-               [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-               [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
-            },
-            maxPreload = 100000,
-            preloadFileSize = 10000,
-         },
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
       },
-   },
+      workspace = {
+        library = {
+          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+          [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
+        },
+        maxPreload = 100000,
+        preloadFileSize = 10000,
+      },
+    },
+  },
 }
-
--- requires a file containing user's lspconfigs
-local addlsp_confs = utils.load_config().plugins.options.lspconfig.setup_lspconf
-
-if #addlsp_confs ~= 0 then
-   require(addlsp_confs).setup_lsp(M.on_attach, capabilities)
-end
-
+lspconfig.clangd.setup { 
+  on_attach = M.on_attach,
+  capabilities = M.capabilities,
+}
+lspconfig.pylsp.setup { }
+lspconfig.jedi_language_server.setup { }
+lspconfig.marksman.setup { }
+lspconfig.lemminx.setup { }
+lspconfig.jsonls.setup { }
+lspconfig.groovyls.setup { }
+lspconfig.gradle_ls.setup { }
+lspconfig.jdtls.setup { 
+  on_attach = M.on_attach,
+  capabilities = M.capabilities,
+  cmd = { 
+    "jdtls", 
+    "-configuration",  "/home/mxlei/.cache/jdtls/config", 
+    "-data", "/home/mxlei/.cache/jdtls/workspace" 
+  },
+  filetypes = { "java" },
+  init_options = {
+    jvm_args = {},
+    workspace = '/home/mxlei/.cache/jdtls/workspace'
+  },
+  single_file_support = true
+}
+lspconfig.kotlin_language_server.setup { 
+  on_attach = M.on_attach,
+  capabilities = M.capabilities
+}
+-- lspconfig.vim_language_server.setup { }
+-- lspconfig.sql_language_server.setup { }
+-- lspconfig.docker_langserver.setup { }
+-- lspconfig.docker_compose_langserver.setup { }
+-- lspconfig.cmake_language_server.setup { }
+-- lspconfig.vscode_json_language_server.setup { }
+lspconfig.bashls.setup { }
+lspconfig.cmake.setup { 
+  on_attach = M.on_attach,
+  capabilities = M.capabilities,
+}
+lspconfig.html.setup { }
+lspconfig.cssls.setup { }
+lspconfig.yamlls.setup { }
+lspconfig.tsserver.setup { }
+lspconfig.dartls.setup { }
+lspconfig.dockerls.setup { }
+lspconfig.docker_compose_language_service.setup { }
+lspconfig.bashls.setup { }
+lspconfig.rust_analyzer.setup { }
 return M
